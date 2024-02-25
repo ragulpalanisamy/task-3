81fcec75ee4e3b523377800df2e5e3047461e594
ragulpalanisamy <ragulpalanisamy1001@gmail.com>
2024-02-25 19:21:34 +0530

    feat: :sparkles: Redux explored

--- multi-step-register/src/App.tsx
+++ multi-step-register/src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css'
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import Redux_learning from './pages/Redux_learning';
+import MultiStep from './pages/Multi-step';
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MultiStep />} />
+          <Route path="/redux-learning" element={<Redux_learning />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
